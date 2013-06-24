44b8bad0845000ded1fb8476d3b5030b6392b4fd
Taylor Yelverton <tny5357@gmail.com>
2013-06-24 02:14:18 -0500

    Setting up base application framework.

---
+++ app/app.js
@@ -0,0 +1,9 @@
+var jengineBuilder = require('jengine_builder');
+
+var JEngineBuilderApplication = module.exports = new jengineBuilder.Application();
+JEngineBuilderApplication.intro();
+
+// var appWindow = module.exports = appjs.createWindow({
+//   width: 640,
+//   height: 460
+// });
